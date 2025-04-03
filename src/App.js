39175e1352e3b3fc11dad532797202d9a6968acd
Vineker1One <106377402+Vineker1One@users.noreply.github.com>
2025-04-03 19:54:33 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from './components/Button';
+import { ReactComponent as GreenIcon } from './Green.svg';
+import { ReactComponent as RedIcon } from './RED.svg';
+import './styles/style.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Сделай ставку</h1>
+      <div className="buttons-container">
+        <Button onClick={() => alert('Ваш голос учтен')}>
+          <GreenIcon />
+        </Button>
+        <Button onClick={() => alert('Ваш голос учтен')}>
+          <RedIcon />
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
